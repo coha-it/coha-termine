4409e3a8d6dfdfac71a8f2009b744651daa48005
Alexander Bachschmid <alexander.bachschmid@prochannel.eu>
2019-11-08 20:44:12 +0100

    Initial Commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,279 @@
+<template>
+  <v-container>
+    
+
+<template>
+  <v-row class="fill-height">
+    <v-col>
+      <v-sheet height="64">
+        <v-toolbar flat color="white">
+          <v-btn outlined class="mr-4" @click="setToday">
+            Heute
+          </v-btn>
+          <v-btn fab text small @click="prev">
+            <v-icon small>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-btn fab text small @click="next">
+            <v-icon small>mdi-chevron-right</v-icon>
+          </v-btn>
+          <v-toolbar-title>{{ title }}</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-icon 
+            v-if="type != 'month'"
+            @click="type = 'month'"
+          >mdi-backup-restore</v-icon>
+          <v-menu bottom right>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                outlined
+                v-on="on"
+              >
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Monat</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Tag</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Woche</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = '4day'">
+                <v-list-item-title>4 Tage</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
+      </v-sheet>
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          v-model="focus"
+          color="primary"
+          locale="de"
+          :events="events"
+          :event-color="getEventColor"
+          :event-margin-bottom="3"
+          :now="today"
+          :type="type"
+          @click:event="showEvent"
+          @click:more="viewDay"
+          @click:date="viewDay"
+          @change="updateRange"
+        ></v-calendar>
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          full-width
+          offset-x
+        >
+          <v-card
+            color="grey lighten-4"
+            min-width="350px"
+            flat
+          >
+            <v-toolbar
+              :color="selectedEvent.color"
+              dark
+            >
+              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn icon target="_blank" :href="'https://corporate-happiness.de/detail/index/sArticle/' + selectedEvent.article_id">
+                  <v-icon>mdi-calendar</v-icon>
+              </v-btn>
+              <!-- <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn> -->
+            </v-toolbar>
+            <v-card-text>
+              <span v-html="selectedEvent.details"></span>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                text
+                color="secondary"
+                @click="selectedOpen = false"
+              >
+                Schließen
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+
+
+  </v-container>
+</template>
+
+<script>
+
+import axios from "axios";
+
+
+  export default {
+    data: () => ({
+      focus: '2019-11-08',
+      today: '2019-11-08',
+      type: 'month',
+      typeToLabel: {
+        month: 'Monat',
+        week: 'Woche',
+        day: 'Tage',
+        '4day': '4 Tage',
+      },
+      start: null,
+      end: null,
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+
+      events: [{"name":"TOP ENTSCHEIDER SEMINAR mit Lisa Ganster", "color": 'blue', "start":"2019-10-19"}],
+      x_events: [
+        {
+          name: 'Vacation',
+          details: 'Going to the beach! <b>bold</b>',
+          start: '2019-10-29',
+          end: '2019-11-04',
+          color: 'blue',
+        },
+        {
+          name: '30th Birthday',
+          details: 'Celebrate responsibly',
+          start: '2019-11-06',
+          color: 'primary',
+        },
+        {
+          name: 'Hackathon',
+          details: 'Code like there is no tommorrow',
+          start: '2019-11-27 23:00',
+          end: '2019-12-01 08:00',
+          color: 'black',
+        },
+
+        {
+          name: 'Viele Termine!',
+          details: 'Viele Termine!!!!',
+          start: '2019-11-15',
+          color: 'teal',
+        },
+        {
+          name: 'Viele Termine!',
+          details: 'Viele Termine!!!!',
+          start: '2019-11-15',
+          color: 'teal',
+        },
+        {
+          name: 'Viele Termine!',
+          details: 'Viele Termine!!!!',
+          start: '2019-11-15',
+          color: 'teal',
+        },
+        {
+          name: 'Viele Termine!',
+          details: 'Viele Termine!!!!',
+          start: '2019-11-15',
+          color: 'teal',
+        },
+
+      ],
+    }),
+    computed: {
+      title () {
+        const { start, end } = this
+        if (!start || !end) {
+          return ''
+        }
+
+        const startMonth = this.monthFormatter(start)
+        const endMonth = this.monthFormatter(end)
+        const suffixMonth = startMonth === endMonth ? '' : endMonth
+
+        const startYear = start.year
+        const endYear = end.year
+        const suffixYear = startYear === endYear ? '' : endYear
+
+        const startDay = start.day + this.nth(start.day)
+        const endDay = end.day + this.nth(end.day)
+
+        switch (this.type) {
+          case 'month':
+            return `${startMonth} ${startYear}`
+          case 'week':
+          case '4day':
+            return `${startMonth} ${startDay} ${startYear} - ${suffixMonth} ${endDay} ${suffixYear}`
+          case 'day':
+            return `${startMonth} ${startDay} ${startYear}`
+        }
+        return ''
+      },
+      monthFormatter () {
+        return this.$refs.calendar.getFormatter({
+          timeZone: 'UTC', month: 'long',
+        })
+      },
+    },
+    mounted () {
+      this.$refs.calendar.checkChange();
+
+      axios.get("/public_events.json").then((response) => {
+        this.events = response.data;
+      }, (error) => {
+        console.log(error);
+      });
+    },
+    methods: {
+
+      viewDay ({ date }) {
+        this.focus = date
+        this.type = 'day'
+      },
+      getEventColor (event) {
+        return event.color
+      },
+      setToday () {
+        this.focus = this.today
+      },
+      prev () {
+        this.$refs.calendar.prev()
+      },
+      next () {
+        this.$refs.calendar.next()
+      },
+      showEvent ({ nativeEvent, event }) {
+        const open = () => {
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          setTimeout(() => this.selectedOpen = true, 10)
+        }
+
+        if (this.selectedOpen) {
+          this.selectedOpen = false
+          setTimeout(open, 10)
+        } else {
+          open()
+        }
+
+        nativeEvent.stopPropagation()
+      },
+      updateRange ({ start, end }) {
+        // You could load events from an outside source (like database) now that we have the start and end dates on the calendar
+        this.start = start
+        this.end = end
+      },
+      nth (d) {
+        return d > 3 && d < 21
+          ? 'th'
+          : ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'][d % 10]
+      },
+    },
+  }
+</script>
