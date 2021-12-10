b933e9d627ba14c80a4d922673754d52fca0deee
Your Name <you@example.com>
2021-12-10 21:42:11 +0100

    File Upload added

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,15 @@ const routes = [
   //   // this generates a separate chunk (about.[hash].js) for this route
   //   // which is lazy-loaded when the route is visited.
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  // },
+  {
+    path: '/file-upload',
+    name: 'FileUpload',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('@/views/FileUpload.vue')
+  },
   {
     path: "/:catchAll(.*)",
     name: 'Calendar',
---
+++ src/views/FileUpload.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+.file-upload
+  v-form(v-model='valid')
+    v-container
+      v-row
+        v-col
+          br
+          v-spacer
+          h1 Datei hochladen
+      v-spacer
+      v-row
+        v-col(cols='12' xs='12' sm='8' md='6')
+          v-text-field(
+            v-model='pin'
+            :rules='pinRules'
+            label='PIN / Passwort'
+            required
+            type="password"
+            outlined
+          )
+      v-spacer
+
+      v-row
+        v-col(cols='12' xs='12' sm='8' md='6')
+          | Event-Datei hochladen
+          v-file-input(
+            accept=".xml"
+            outlined
+            label="XML-Datei auswählen"
+            show-size
+            prepend-icon="mdi-file-document-outline"
+          )
+          v-spacer
+      v-row
+        v-col
+          v-btn(
+            color="primary"
+            depressed
+            :disabled="!valid || pin.length < 3"
+          ) Hochladen
+</template>
+
+<script>
+export default {
+  name: "FileUpload",
+
+  data: () => ({
+    valid: true,
+    pin: '',
+    pinRules: [
+      v => !!v || 'PIN ist erforderlich'
+    ]
+  }),
+
+};
+</script>
