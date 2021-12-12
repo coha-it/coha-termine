b7109609a68a6e50a2ca670c526ec8ef2a6e3831
Your Name <you@example.com>
2021-12-12 19:27:52 +0100

    DE locale

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
+// Translation provided by Vuetify (javascript)
+import de from 'vuetify/lib/locale/de'
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  lang: {
+    locales: { de },
+    current: 'de',
+  },
 });
