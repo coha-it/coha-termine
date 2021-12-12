21f4c534ea5fcc85ee8363b2b2f88c0bbfa86f91
Your Name <you@example.com>
2021-12-12 17:52:27 +0100

    HTML to PUG

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,102 +1,60 @@
-<template>
-  <v-container>
-    <template>
-      <v-row class="fill-height">
-        <v-col>
-          <v-sheet height="64">
-            <v-toolbar flat color="white">
-              <v-btn outlined class="mr-4" @click="setToday"> Heute </v-btn>
-              <v-btn fab text small @click="prev">
-                <v-icon small>mdi-chevron-left</v-icon>
-              </v-btn>
-              <v-btn fab text small @click="next">
-                <v-icon small>mdi-chevron-right</v-icon>
-              </v-btn>
-              <v-toolbar-title>{{ title }}</v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-icon v-if="type != 'month'" @click="type = 'month'">mdi-backup-restore</v-icon>
-              <v-menu bottom right>
-                <template v-slot:activator="{ on }">
-                  <v-btn outlined v-on="on">
-                    <span>{{ typeToLabel[type] }}</span>
-                    <v-icon right>mdi-menu-down</v-icon>
-                  </v-btn>
-                </template>
-                <v-list>
-                  <v-list-item @click="type = 'month'">
-                    <v-list-item-title>Monat</v-list-item-title>
-                  </v-list-item>
-                  <v-list-item @click="type = 'day'">
-                    <v-list-item-title>Tag</v-list-item-title>
-                  </v-list-item>
-                  <v-list-item @click="type = 'week'">
-                    <v-list-item-title>Woche</v-list-item-title>
-                  </v-list-item>
-                  <v-list-item @click="type = '4day'">
-                    <v-list-item-title>4 Tage</v-list-item-title>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </v-toolbar>
-          </v-sheet>
-          <v-sheet height="600" v-if="events">
-            <v-calendar
-              ref="calendar"
-              v-model="focus"
-              color="primary"
-              locale="de"
-              :events="events"
-              :event-color="getEventColor"
-              :event-margin-bottom="3"
-              :now="today"
-              :type="type"
-              :weekdays="[1, 2, 3, 4, 5, 6, 0]"
-              @click:event="showEvent"
-              @click:more="viewDay"
-              @click:date="viewDay"
-              @change="updateRange"
-            ></v-calendar>
-            <v-menu
-              v-model="selectedOpen"
-              :close-on-content-click="false"
-              :activator="selectedElement"
-              offset-x
-            >
-              <v-card color="grey lighten-4" min-width="350px" flat>
-                <v-toolbar color="secondary" dark>
-                  <v-toolbar-title
-                    v-html="selectedEvent.name"
-                  ></v-toolbar-title>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    icon
-                    target="_blank"
-                    :href="
-                      'https://corporate-happiness.de/detail/index/sArticle/' +
-                      selectedEvent.article_id
-                    "
-                  >
-                    <v-icon>mdi-calendar</v-icon>
-                  </v-btn>
-                  <!-- <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn> -->
-                </v-toolbar>
-                <v-card-text>
-                  <span v-html="selectedEvent.details"></span>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn text color="secondary" @click="selectedOpen = false">
-                    Schließen
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-menu>
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </template>
-  </v-container>
+<template lang="pug">
+v-container
+  template
+    v-row.fill-height
+      v-col
+        v-sheet(height='64')
+          v-toolbar(flat color='white')
+            v-btn.mr-4(outlined @click='setToday')  Heute 
+            v-btn(fab text small @click='prev')
+              v-icon(small) mdi-chevron-left
+            v-btn(fab text small @click='next')
+              v-icon(small) mdi-chevron-right
+            v-toolbar-title {{ title }}
+            v-spacer
+            v-icon(v-if="type != 'month'" @click="type = 'month'") mdi-backup-restore
+            v-menu(bottom right)
+              template(v-slot:activator='{ on }')
+                v-btn(outlined v-on='on')
+                  span {{ typeToLabel[type] }}
+                  v-icon(right) mdi-menu-down
+              v-list
+                v-list-item(@click="type = 'month'")
+                  v-list-item-title Monat
+                v-list-item(@click="type = 'day'")
+                  v-list-item-title Tag
+                v-list-item(@click="type = 'week'")
+                  v-list-item-title Woche
+                v-list-item(@click="type = '4day'")
+                  v-list-item-title 4 Tage
+        v-sheet(height='600' v-if='events')
+          v-calendar(
+            ref='calendar'
+            v-model='focus'
+            color='primary'
+            locale='de'
+            :events='events'
+            :event-color='getEventColor'
+            :event-margin-bottom='3'
+            :now='today'
+            :type='type'
+            :weekdays='[1, 2, 3, 4, 5, 6, 0]'
+            @click:event='showEvent'
+            @click:more='viewDay'
+            @click:date='viewDay'
+            @change='updateRange'
+          )
+          v-menu(v-model='selectedOpen' :close-on-content-click='false' :activator='selectedElement' offset-x)
+            v-card(color='grey lighten-4' min-width='350px' flat)
+              v-toolbar(color='secondary' dark)
+                v-toolbar-title(v-html='selectedEvent.name')
+                v-spacer
+                v-btn(icon target='_blank' :href="selectedEvent.article_url")
+                  v-icon mdi-calendar
+              v-card-text
+                span(v-html='selectedEvent.details')
+              v-card-actions
+                v-btn(text color='secondary' @click='selectedOpen = false') Schlie&szlig;en
 </template>
 
 <script>
