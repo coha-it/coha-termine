43c6c08d9589f2a16545405ebe8e98b9b13a6916
Your Name <you@example.com>
2021-12-29 15:07:07 +0100

    color  fixes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,11 +14,16 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: '#3f51b5',
-        secondary: '#b0bec5',
-        accent: '#8c9eff',
+        primary: '#758589',
+        secondary: '#afc5cb',
+        accent: '#efd84d',
         error: '#b71c1c',
         success: '#26a151',
+        // primary: '#3f51b5',
+        // secondary: '#b0bec5',
+        // accent: '#8c9eff',
+        // error: '#b71c1c',
+        // success: '#26a151',
       },
     },
   },
